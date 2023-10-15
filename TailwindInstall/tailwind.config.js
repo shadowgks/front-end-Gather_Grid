73bcfe33e56cf61266e5935108ec98f2a900944a
saad moumou <saad.moumou.coder@gmail.com>
2023-10-15 23:04:28 +0100

    fix file dashboard not working because tailwind config not added this **/ in the path content

--- TailwindInstall/tailwind.config.js
+++ TailwindInstall/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./dist/*.{html,js}",
+    "./dist/**/*.{html,js}",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
