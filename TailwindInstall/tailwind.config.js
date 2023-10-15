f475bf4423b53c5bd215e2e77d6fb3710bab3226
saad moumou <saad.moumou.coder@gmail.com>
2023-10-15 10:56:08 +0100

    add categorys and cards events

--- TailwindInstall/tailwind.config.js
+++ TailwindInstall/tailwind.config.js
@@ -7,15 +7,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#1A1A1D',
+        'primary': {
+          50: '#4E4E50',
+          100: '#1A1A1D',
+        },
         'second': {
           50: '#66FCF1',
           100: '#45A29E'
         },
       },
+
       spacing: {
         '100': '500px',
-      }
+      },
+
+      fontFamily: {
+        Chau: ['Chau Philomene One', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif']
+      },
     },
   },
   plugins: [require("flowbite")],
